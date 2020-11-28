74c614846306e366612cbb926b5c2993431df127
@slovacus <slovacus@gmail.com>
2020-11-28 01:06:52 -0500

    refactor: load starship for zsh (#21)

--- zsh-starship.zsh
+++ zsh-starship.zsh
@@ -7,53 +7,14 @@
 # Authors:
 #   Luis Mayta <slovacus@gmail.com>
 #
-starship_package_name=starship
-ZSH_STARSHIP_ROOT=$(dirname "${0}")
-ZSH_STARSHIP_CONF_DIR="${ZSH_STARSHIP_ROOT}/config"
-ZSH_HOME_CONF_DIR="${HOME}/.config"
 
+ZSH_STARSHIP_PATH=$(dirname "${0}")
 
-function starship::dependences {
-    message_info "Installing dependences for $starship_package_name"
-    if ! type -p brew > /dev/null; then
-        message_warning "it's neccesary install brew, please use 'antibody bundle luismayta/zsh-brew branch:develop'"
-        return
-    fi
-    message_success "Installed dependences for $starship_package_name"
-}
+# shellcheck source=/dev/null
+source "${ZSH_STARSHIP_PATH}"/config/main.zsh
 
-function starship::packages {
-    message_info "Install packages for $starship_package_name"
-    message_success "Installed packages for $starship_package_name"
-}
+# shellcheck source=/dev/null
+source "${ZSH_STARSHIP_PATH}"/internal/main.zsh
 
-function starship::install {
-    starship::dependences
-    message_info "Installing $starship_package_name"
-    brew install starship
-    message_success "Installed $starship_package_name"
-}
-
-function starship::post_install {
-    message_info "Post Install $starship_package_name"
-    starship::sync
-    message_success "Success Install $starship_package_name"
-}
-
-function starship::sync {
-    rsync -avzh --progress "${ZSH_STARSHIP_CONF_DIR}/" "${ZSH_HOME_CONF_DIR}/"
-}
-
-
-function starship::load {
-    if type -p starship > /dev/null; then
-        eval "$(starship init zsh)"
-    fi
-}
-
-starship::load
-
-if ! type -p starship > /dev/null; then
-    starship::install
-    starship::post_install
-fi
+# shellcheck source=/dev/null
+source "${ZSH_STARSHIP_PATH}"/pkg/main.zsh
