91ad132c136ad2f25264476c3d5ed7256fb6bdfd
@slovacus <slovacus@gmail.com>
2021-02-16 22:59:50 -0500

    perf: name functions zsh (#24)

--- internal/osx.zsh
+++ internal/osx.zsh
@@ -1,14 +1,2 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
-
-function starship::internal::reattach-to-user-namespace::install {
-    if ! type -p brew > /dev/null; then
-        message_warning "${STARSHIP_MESSAGE_BREW}"
-        return
-    fi
-    message_info "Installing reattach-to-user-namespace for ${STARSHIP_PACKAGE_NAME}"
-    brew install reattach-to-user-namespace
-    message_success "Installed reattach-to-user-namespace ${STARSHIP_PACKAGE_NAME}"
-}
-
-if ! type -p reattach-to-user-namespace > /dev/null; then starship::internal::reattach-to-user-namespace::install; fi
