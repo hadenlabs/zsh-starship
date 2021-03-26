b68aacc8883563a67e19f4068ebbdcb52d22f1ad
Luis Mayta <luis@hadenlabs.com>
2021-03-26 11:20:32 -0500

    style: format code zsh (#32)

--- pkg/main.zsh
+++ pkg/main.zsh
@@ -14,7 +14,6 @@ function starship::pkg::main::factory {
         source "${ZSH_STARSHIP_PATH}"/pkg/linux.zsh
       ;;
     esac
-
     # shellcheck source=/dev/null
     source "${ZSH_STARSHIP_PATH}"/pkg/helper.zsh
 
